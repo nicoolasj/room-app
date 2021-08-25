4621e51701304027af9fad65802dc856bbd5c076
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-08-25 16:52:59 -0300

    create ItemAdapter adapter class

---
+++ app/src/main/java/com/example/roomapp/adapter/ItemAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.roomapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomapp.R
+import com.example.roomapp.model.Item
+
+class ItemAdapter(private val listener: OnItemClickListener) :
+    RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    private var itemData = emptyList<Item>()
+
+    interface OnItemClickListener {
+        fun onItemClick()
+        fun onItemLongClick()
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener,
+        View.OnLongClickListener {
+
+        init {
+            view.setOnClickListener(this)
+            view.setOnLongClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = absoluteAdapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick()
+            }
+        }
+
+        override fun onLongClick(v: View?): Boolean {
+            val position = absoluteAdapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemLongClick()
+            }
+            return true
+        }
+
+        val textView: TextView = view.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = itemData[position].item
+    }
+
+    override fun getItemCount(): Int = itemData.size
+
+    fun itemList(itemData: List<Item>) {
+        this.itemData = itemData
+        notifyDataSetChanged()
+    }
+}
